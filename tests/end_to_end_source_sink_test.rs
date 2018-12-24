41ea79396cffd28c08d075c6cedff934ac5b3a42
ucyo <cayoglu@me.com>
2018-12-24 13:37:59 +0100

    Add first test for reading from stdin

--- tests/end_to_end_source_sink_test.rs
+++ tests/end_to_end_source_sink_test.rs
@@ -1,2 +1,14 @@
 /// First walking skeleton for testing
 ///
+
+extern crate pzip;
+
+#[test]
+
+fn read_from_stdin_to_memory_and_write_to_stdout() {
+    let source = pzip::Source::New('-');
+    let sink   = pzip::Sink::New('-');
+
+    source.memory_read();
+    source.write(&sink);
+}
