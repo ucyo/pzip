4905da61222df3551c85c5015f39dc4b28601634
ucyo <cayoglu@me.com>
2018-12-24 14:04:32 +0100

    Accept Strings as input for setting up the source and sink

--- tests/end_to_end_source_sink_test.rs
+++ tests/end_to_end_source_sink_test.rs
@@ -6,9 +6,9 @@ extern crate pzip;
 #[test]
 
 fn read_from_stdin_to_memory_and_write_to_stdout() {
-    let source = pzip::Source::New('-');
-    let sink   = pzip::Sink::New('-');
+    let source = pzip::Source::new("-".to_string());
+    let sink   = pzip::Sink::new("-".to_string());
 
-    source.memory_read();
-    source.write(&sink);
+    source.memory_read().unwrap();
+    source.write(&sink).unwrap();
 }
