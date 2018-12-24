cb1c75241dd241882fc1cad9b2d7ce4752dc2e8a
ucyo <cayoglu@me.com>
2018-12-24 14:05:03 +0100

    First walking skeleton, time to make test pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,45 @@
+/// pzip - predicted zip
+///
+/// # pzip
+/// A compression library for floating point data
+
+#[derive(Debug)]
+pub struct Source {
+    source: String,
+}
+
+#[derive(Debug)]
+pub struct Sink {
+    sink: String,
+}
+
+impl Source {
+
+    pub fn new(source: String) -> Source {
+        Source { source }
+    }
+
+    pub fn memory_read(&self) -> Result<(), String>{
+        panic!("Reading into memory did not work.")
+    }
+
+    pub fn write(&self, sink: &Sink) -> Result<(), String>{
+        panic!("Writing into sink did not work.")
+    }
+}
+
+impl Sink {
+
+    pub fn new(sink: String) -> Sink {
+        Sink { sink }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;  // add lib into scope
     #[test]
+    #[ignore]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
