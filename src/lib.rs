96cfd926605f8c37eb394b5d977bf396b709e33c
ucyo <cayoglu@me.com>
2018-12-31 15:47:43 +0100

    Implement byte operation with Traits instead of struct methods

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,74 @@ pub struct Sink<T> {
     file : fs::File,
     data : PhantomData<T>
 }
+
+pub trait FileToBeCompressed<T> {
+    fn new(filename: &String) -> Self;
+    fn ix(&self, position: usize) -> &T;
+    fn get(&mut self) -> T;
+    fn load(&mut self) -> Result<usize, io::Error>;
+}
+
+pub trait CompressedFile<T> {
+    fn new(filename: &String) -> Self;
+    fn flush(&mut self) -> Result<(), io::Error>;
+    fn put(&mut self, value: T) -> Result<(), io::Error>;
+    fn put_all(&mut self, value: &[T])  -> Result<(), io::Error>;
+}
+
+pub struct So<T> {file: fs::File, data: Vec<T>}
+pub struct Si<T> {file: fs::File, data: PhantomData<T>}
+
+
+impl FileToBeCompressed<u8> for So<u8> {
+
+    fn new(filename: &String) -> Self {
+        let file = fs::File::open(filename).unwrap();
+        let data : Vec<u8> = Vec::new();
+        So{file, data}
+    }
+
+    fn ix(&self, position: usize) -> &u8 {
+        &self.data[position]
+    }
+
+    fn get(&mut self) -> u8 {
+        self.file.read_u8().unwrap()
+    }
+
+    fn load(&mut self) -> Result<usize, io::Error>{
+        let mut bytes : Vec<u8> = Vec::new();
+        let length = self.file.read_to_end(&mut bytes)?;
+        self.data = bytes;
+        Ok(length)
+    }
+
+}
+
+impl CompressedFile<u8> for Si<u8> {
+
+    // REFACTOR: Change filename to fs::path::Path type
+    fn new(filename: &String) -> Self {
+        let file = fs::File::create(&filename).unwrap();
+        Si{file, data: PhantomData}
+    }
+
+    fn flush(&mut self)  -> Result<(), io::Error> {
+       self.file.flush()?;
+       Ok(())
+    }
+
+    fn put(&mut self, value: u8) -> Result<(), io::Error> {
+        self.file.write_u8(value)?;
+        Ok(())
+    }
+
+    fn put_all(&mut self, values: &[u8])  -> Result<(), io::Error> {
+        self.file.write(values)?;
+        Ok(())
+    }
+}
+
 impl<T> Source<T> {
 
     // REFACTOR: Change filename to fs::path::Path type
---
+++ tests/test_write_read_trait_u8.rs
@@ -0,0 +1,64 @@
+/// First walking skeleton for testing
+///
+
+use pzip::CompressedFile;
+use pzip::FileToBeCompressed;
+
+
+#[test]
+fn trait_write_byte_to_file() {
+
+    let filename = String::from("/tmp/output.raw");
+    let v = 213_u8;
+
+    let mut sink: pzip::Si<u8> = pzip::Si::new(&filename);
+    sink.put(v).expect("Writing unsuccessfull");
+    sink.flush().expect("Writing unsuccessfull");
+
+    let mut source: pzip::Source<u8> = pzip::Source::new(&filename).expect("Error");
+    let value = source.get();
+
+    std::fs::remove_file(&filename).expect("Error");
+    assert_eq!(v, value)
+}
+
+#[test]
+fn trait_write_bytes_to_file() {
+    let filename = String::from("/tmp/output.raw");
+    let values = [123_u8, 193_u8, 201_u8];
+
+    let mut sink: pzip::Si<u8> = pzip::Si::new(&filename);
+    sink.put_all(&values).expect("Writing unsuccessfull");
+    sink.flush().expect("Writing unsuccessfull");
+
+    let mut source: pzip::Source<u8> = pzip::Source::new(&filename).expect("Error");
+    source.load().expect("Load unsuccessfull");
+
+    for i in 0..3 {
+        assert_eq!(source.ix(i), &values[i])
+    }
+    std::fs::remove_file(&filename).expect("Error");
+}
+
+
+#[test]
+fn trait_read_first_byte_from_file() {
+    let filename = "/home/ucyo/Developments/big_files/subset.raw".to_string();
+
+    let mut source: pzip::So<u8> = pzip::So::new(&filename);
+    let first = source.get();
+    assert_eq!(first, 166u8)
+}
+
+#[test]
+fn trait_read_bytes_from_file() {
+    let filename = "/home/ucyo/Developments/big_files/subset.raw".to_string();
+
+    let mut source: pzip::So<u8> = pzip::So::new(&filename);
+    source.load().expect("Error loading the data");
+
+    let expected = [166_u8, 146_u8, 32_u8];
+    for i in 0..3 {
+        assert_eq!(source.ix(i), &expected[i]);
+    }
+}
