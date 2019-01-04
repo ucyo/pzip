9108d43cf2b184733ce18e1a075ddc9c4066b544
ucyo <cayoglu@me.com>
2019-01-04 16:09:06 +0100

    Add benchmarking method for prediction.
    - There seems to be no difference in using an iterator or not.

--- benches/pzip_benchmark.rs
+++ benches/pzip_benchmark.rs
@@ -7,17 +7,47 @@
 extern crate criterion;
 
 use criterion::Criterion;
+use pzip::traversal::{Traversal, predictions, Predictor, GeneratorIteratorAdapter};
+use pzip::position::Position;
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
+
+fn get_all_predictions()
+{
+    let data = vec![  0.0,  1.0,  2.0,
+                    3.0,  4.0,  5.0,
+                    6.0,  7.0,  8.0,
+
+                    9.0, 10.0, 11.0,
+                    12.0, 13.0, 14.0,
+                    15.0, 16.0, 17.0,
+
+                    18.0, 19.0, 20.0,
+                    21.0, 22.0, 23.0,
+                    24.0, 25.0, 26.0,
+                    ];
+    let tr = Traversal::new(3, 3, 3);
+
+    let mut weights: Vec<(i32, Position)> = Vec::new();
+    weights.push( ( 1, Position{x:1,y:0,z:0} ) );
+    weights.push( ( 1, Position{x:1,y:1,z:0} ) );
+    weights.push( ( 1, Position{x:1,y:0,z:1} ) );
+    weights.push( ( 1, Position{x:0,y:1,z:1} ) );
+    weights.push( ( 1, Position{x:1,y:0,z:1} ) );
+    weights.push( ( 1, Position{x:0,y:0,z:1} ) );
+    weights.push( ( 1, Position{x:0,y:1,z:0} ) );
+
+    let p = Predictor{
+        traversal: tr,
+        weights: weights,
+        data: data,
+    };
+
+    let generator_iterator = GeneratorIteratorAdapter(predictions(p));
+    let _ : Vec<f64> = generator_iterator.collect();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(20)));
+    c.bench_function("all_pred", |b| b.iter(|| get_all_predictions()));
 }
 
 criterion_group!(benches, criterion_benchmark);
