dd1a86f8cadfec7aa208292d7a1e278c8e11863e
ucyo <cayoglu@me.com>
2019-01-05 13:10:29 +0100

    [Test] Add tests for Ordered mapping function

--- src/mapping.rs
+++ src/mapping.rs
@@ -24,34 +24,75 @@ mod tests {
     use super::*;
 
     #[test]
+    fn correct_mapping() {
+        let values   = vec![
+            33832.8f32,
+            -33832.8f32,
+            1f32,
+            -1f32,
+            0f32,
+        ];
+
+        let expected_ordered = vec![
+            0xc70428cd,
+            !0xc70428cd,
+            0xbf800000,
+            !0xbf800000,
+            0x80000000,
+        ];
+
+        for (v,e) in values.iter().zip(expected_ordered) {
+            assert_eq!(Ordered::to_u32(*v), e);
+        }
+    }
+
+    #[test]
     fn transform_f32_to_u32() {
-        let tests = vec![32f32,85.2934f32,8393.42f32];
+        let tests = vec![32f32,85.2934f32,8393.42f32,-53f32,
+                         -0f32, 0f32, std::f32::INFINITY, std::f32::NAN];
         for val in tests {
-            assert_eq!(Raw::from_u32(Raw::to_u32(val)), val)
+            if val.is_nan(){
+                assert!(Raw::from_u32(Raw::to_u32(val)).is_nan());
+                assert!(Ordered::from_u32(Ordered::to_u32(val)).is_nan());
+                continue
+            }
+            assert_eq!(Raw::from_u32(Raw::to_u32(val)), val);
+            assert_eq!(Ordered::from_u32(Ordered::to_u32(val)), val);
         }
     }
 
     #[test]
     fn transform_f64_to_u64() {
-        let tests = vec![32.3352,85.2934,8393.42];
+        let tests = vec![32.3352,85.2934,8393.42,0.0, -0.0,
+                         std::f64::INFINITY, std::f64::NAN];
         for val in tests {
-            assert_eq!(Raw::from_u64(Raw::to_u64(val)), val)
+            if val.is_nan() {
+                assert!(Raw::from_u64(Raw::to_u64(val)).is_nan());
+                assert!(Ordered::from_u64(Ordered::to_u64(val)).is_nan());
+                continue
+            }
+            assert_eq!(Raw::from_u64(Raw::to_u64(val)), val);
+            assert_eq!(Ordered::from_u64(Ordered::to_u64(val)), val);
         }
     }
 
     #[test]
     fn transform_u32_to_f32() {
-        let tests = vec![32u32,85u32,293u32,83u32,9342u32,1u32];
+        let tests = vec![32u32,85u32,293u32,83u32,
+                         9342u32,1u32,0u32,984744474u32];
         for val in tests {
-            assert_eq!(Raw::to_u32(Raw::from_u32(val)), val)
+            assert_eq!(Raw::to_u32(Raw::from_u32(val)), val);
+            assert_eq!(Ordered::to_u32(Ordered::from_u32(val)), val);
         }
     }
 
     #[test]
     fn transform_u64_to_f64() {
-        let tests = vec![32u64,3352u64,8u64,529u64,34u64,83u64,93u64,42u64,];
+        let tests = vec![32u64,3352u64,8u64,529u64,
+                         34u64,83u64,93u64,42u64,984742123298754474u64];
         for val in tests {
-            assert_eq!(Raw::to_u64(Raw::from_u64(val)), val)
+            assert_eq!(Raw::to_u64(Raw::from_u64(val)), val);
+            assert_eq!(Ordered::to_u64(Ordered::from_u64(val)), val);
         }
     }
 }
