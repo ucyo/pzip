a75c72fdce5868f6af6bbb1d25b83d03a79b5e94
ucyo <cayoglu@me.com>
2019-01-05 13:12:08 +0100

    [Pass] Add Ordered mapping struct
     - Which preserves the ordering of the elements

--- src/mapping.rs
+++ src/mapping.rs
@@ -19,7 +19,58 @@ pub struct Raw {}
 
 impl Mapping for Raw {}
 
+pub struct Ordered {}
 
+impl Mapping for Ordered {
+    fn to_u32(from: f32) -> u32 {
+        let mut result = unsafe {std::mem::transmute::<f32, u32>(from)};
+        result = if from < 0f32 {
+            !result
+        } else {
+            result | (1 << 31)
+        };
+
+        // If a value falls within this if-clause, it would
+        // be mapped to a NaN value. To prevent this and to accept
+        // arbitary u32 values, the result needs to be negated. Preliminary
+        // results show that this does not have any side-effects (espacially
+        // for transformations done from proper f32 source)
+        if result > 0x7F80_0000 && from.is_nan() {
+            return !result
+        }
+        result
+    }
+    fn from_u32(from: u32) -> f32 {
+        if from < (1<<31) {
+            return unsafe {std::mem::transmute::<u32, f32>(!from)};
+        } else {
+            return unsafe {std::mem::transmute::<u32, f32>(from - (1<<31))};
+        }
+    }
+    fn to_u64(from: f64) -> u64 {
+        let mut result = unsafe {std::mem::transmute::<f64, u64>(from)};
+        result = if from < 0f64 {
+            !result
+        } else {
+            result | (1 << 63)
+        };
+
+        // see comment @to_u32()
+        if result > 0x7FC0_0000_0000_0000 && from.is_nan() {
+            return !result
+        }
+        result
+    }
+    fn from_u64(from: u64) -> f64 {
+        if from < (1<<63) {
+            return unsafe {std::mem::transmute::<u64, f64>(!from)}
+        } else {
+            return unsafe {std::mem::transmute::<u64, f64>(from - (1<<63))};
+        }
+    }
+}
+
+#[allow(unused_imports)]
 mod tests {
     use super::*;
 
