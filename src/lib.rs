08495eb2f35c4011996610f83d9b41ac6b77a0ad
ucyo <cayoglu@me.com>
2019-01-06 22:22:08 +0100

    Declare functions for further development to pass the tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,31 @@ pub mod mapping;
 pub mod position;
 pub mod testing;
 pub mod traversal;
+
+#[allow(dead_code)]
+pub struct Shape {
+    x: usize,
+    y: usize,
+    z: usize
+}
+
+pub type Position = position::Position;
+
+#[allow(dead_code)]
+pub struct Setup<T> {
+    source: testing::Source<T>,
+    shape: Shape,
+    weights: Vec<(i32, position::Position)>
+}
+
+impl<T> Setup<T> {
+    pub fn new(input: &String, shape: Shape, weights: Vec<(i32, position::Position)>) {
+        unimplemented!();
+    }
+    pub fn write(output: &String) {
+        unimplemented!();
+    }
+    pub fn write_bytes(output: &String) {
+        unimplemented!();
+    }
+}
