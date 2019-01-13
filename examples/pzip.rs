759e7661e26293f7d77827dc6e9d14a6a35adf02
ucyo <cayoglu@me.com>
2019-01-13 15:40:19 +0100

    [example] Create a running binary for pzip

---
+++ examples/pzip.rs
@@ -0,0 +1,26 @@
+extern crate pzip;
+
+use std::env;
+use pzip::{Setup, Weight};
+use pzip::position::Position;
+use pzip::config::FileType;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let configuration = pzip::config::parse_args(&args);
+
+    let lv_weights = vec![Weight {
+        coeff: 1,
+        pos: Position { z: 0, y: 0, x: 1 },
+    }];
+
+    if configuration.filetype == FileType::F64 {
+        let setup = Setup::<f64>::new(configuration.input, configuration.shape, lv_weights);
+        setup.write(configuration.output);
+    } else if configuration.filetype == FileType::F32 {
+        let setup = Setup::<f32>::new(configuration.input, configuration.shape, lv_weights);
+        setup.write(configuration.output);
+    } else {
+        panic!("Error")
+    }
+}
