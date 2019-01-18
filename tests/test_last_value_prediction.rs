be9980b693c8063fa07037220301e033a2d7bc47
ucyo <cayoglu@me.com>
2019-01-18 11:15:19 +0100

    [Test] Add tests for raw mapping and difference writing

--- tests/test_last_value_prediction.rs
+++ tests/test_last_value_prediction.rs
@@ -1,8 +1,9 @@
 use pzip::position::Position;
 use pzip::{Setup, Weight};
+use pzip::mapping::{Raw, Intermapping};
 
 #[test]
-fn compression_using_last_value_all_once_f64() {
+fn compression_using_last_value_all_once_f64_raw() {
     let input = String::from("/home/ucyo/rust/pzip/data/pres.1-90-181-361.f64.bin");
     let output = String::from("/home/ucyo/rust/pzip/data/lastvalue.all.f64.pred");
     let shape = pzip::Shape {
@@ -16,20 +17,20 @@ fn compression_using_last_value_all_once_f64() {
     }];
 
     let prediction = Setup::<f64>::new(&input, shape, weights);
-    prediction.write(&output);
+    prediction.write::<Raw>(&output);
 
     let origin = pzip::testing::read_first_k_f64(&input, 760);
     let outcome = pzip::testing::read_first_k_f64(&output, 760);
 
     for i in 362..623 {
         println!("{} {} {}", i, origin[i - 1], outcome[i]);
-        assert_eq!(origin[i - 1], outcome[i]);
+        assert_eq!(Raw::from_u64(Raw::to_u64(origin[i - 1]) ^ Raw::to_u64(origin[i])), outcome[i]);
     }
     std::fs::remove_file(&output).expect("Error");
 }
 
 #[test]
-fn compression_using_last_value_all_once_f32() {
+fn compression_using_last_value_all_once_f32_raw() {
     let input = String::from("/home/ucyo/rust/pzip/data/pres.1-90-181-361.f32.bin");
     let output = String::from("/home/ucyo/rust/pzip/data/lastvalue.all.f32.pred");
     let shape = pzip::Shape {
@@ -43,14 +44,14 @@ fn compression_using_last_value_all_once_f32() {
     }];
 
     let prediction = Setup::<f32>::new(&input, shape, weights);
-    prediction.write(&output);
+    prediction.write::<Raw>(&output);
 
     let origin = pzip::testing::read_first_k_f32(&input, 760);
     let outcome = pzip::testing::read_first_k_f32(&output, 760);
 
     for i in 362..623 {
         println!("{} {} {}", i, origin[i - 1], outcome[i]);
-        assert_eq!(origin[i - 1], outcome[i]);
+        assert_eq!(Raw::from_u32(Raw::to_u32(origin[i - 1]) ^ Raw::to_u32(origin[i])), outcome[i]);
     }
     std::fs::remove_file(&output).expect("Error");
 }
