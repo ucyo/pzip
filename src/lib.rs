bcaab7109e3a1cd5258b058316bf28c001831667
ucyo <cayoglu@me.com>
2019-01-18 11:15:56 +0100

    [Pass] Pass test for raw mapping and difference writing

--- src/lib.rs
+++ src/lib.rs
@@ -56,12 +56,17 @@ impl Setup<f64> {
         } // fix for f32
     }
 
-    pub fn write(self, output: &String) -> () {
+    pub fn write<H: mapping::Intermapping>(self, output: &String) -> () {
         let mut p = self.to_predictor();
         let generator_iterator = GeneratorIteratorAdapter(predictions(&mut p));
         let results: Vec<f64> = generator_iterator.collect();
+        let diff: Vec<f64> = results.iter()
+                                    .zip(p.data.iter())
+                                    .map(|(a,b)| H::to_u64(*a) ^ H::to_u64(*b))  // TODO eliminate dereferencing
+                                    .map(|x| H::from_u64(x))
+                                    .collect();
         let mut tmp: Vec<u8> = Vec::new();
-        for n in results {
+        for n in diff {
             let _ = tmp.write_f64::<LittleEndian>(n);
         }
         use byteorder::{LittleEndian, WriteBytesExt};
@@ -93,12 +98,18 @@ impl Setup<f32> {
         } // fix for f32
     }
 
-    pub fn write(self, output: &String) -> () {
+    pub fn write<H: mapping::Intermapping>(self, output: &String) -> () {
         let mut p = self.to_predictor();
         let generator_iterator = GeneratorIteratorAdapter(predictions(&mut p));
         let results: Vec<f32> = generator_iterator.collect();
+        let diff: Vec<f32> = results.iter()
+                                    .zip(p.data.iter())
+                                    .map(|(a,b)| H::to_u32(*a) ^ H::to_u32(*b)) // TODO eliminate dereferencing
+                                    .map(|x| H::from_u32(x))
+                                    .collect();
+
         let mut tmp: Vec<u8> = Vec::new();
-        for n in results {
+        for n in diff {
             let _ = tmp.write_f32::<LittleEndian>(n);
         }
         use byteorder::{LittleEndian, WriteBytesExt};
