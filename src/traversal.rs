2434411a104d911a4af402bb45e7524cfb768004
ucyo <cayoglu@me.com>
2019-01-31 16:05:49 +0100

    Add more tests

--- src/traversal.rs
+++ src/traversal.rs
@@ -596,9 +596,31 @@ mod tests {
         }
     }
 
+
     #[test]
     fn test_negative_distance_groups() {
+        let data = vec![
+            0.0, 1.0, 2.0,
+            3.0, 4.0, 5.0,
+            6.0, 7.0, 8.0,
 
+            9.0, 10.0, 11.0,
+            12.0, 13.0, 14.0,
+            15.0,16.0, 17.0,
+
+            18.0, 19.0, 20.0,
+            21.0, 22.0, 23.0,
+            24.0, 25.0, 26.0,
+        ];
+        {   let tr = Traversal::new(3, 3, 3);
+            let mut weights: Vec<Position> = Vec::new();
+            weights.push(Position { x: 2, y: 1, z: 1 });
+            weights.push(Position { x: -2, y: 1, z: 1 });
+
+            let result: Vec<Vec<f64>> = GeneratorIteratorAdapter(neighbours(tr, &data, &weights)).collect();
+
+            assert_eq!(result[24], vec![0.0,14.0]);
+        }
     }
 
     #[test]
