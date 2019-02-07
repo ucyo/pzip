990abb37c89cfa13851597b6b48c268e57377035
ucyo <cayoglu@me.com>
2019-02-07 20:05:38 +0100

    [test] Setting up generic iterator

--- src/ptraversal.rs
+++ src/ptraversal.rs
@@ -1,4 +1,5 @@
 use super::position::Position as Coordinate;
+use super::traversal::GeneratorIteratorAdapter;
 
 pub fn neighbours(shape: &Coordinate, pos: &Vec<Coordinate>, data: &Vec<f32>, ring: bool) -> Vec<Vec<f32>>{
     if ring {
@@ -136,10 +137,20 @@ pub fn get_values_with_default_at_nonexistent_neighbours(shape: &Coordinate, pos
     result
 }
 
+
+use std::ops::{AddAssign, Mul};
+use std::ops::{Generator, GeneratorState};
+pub fn single_neighbours<'a, T: AddAssign<<T as Mul>::Output> + Copy + Default + Mul + From<i16>>(
+    shape: &'a Coordinate, pos: &'a Coordinate, data: &'a Vec<T>, ring: bool) -> impl Generator<Yield = T, Return = ()> + 'a {
+    move || {
+        yield data[0]
+    }
+}
+
 #[allow(unused_imports)]
 mod tests {
     use super::super::{Position};
-    use super::{neighbours};
+    use super::{neighbours, GeneratorIteratorAdapter, single_neighbours};
     #[test]
     fn test_artificial_diff() {
 
@@ -166,6 +177,16 @@ mod tests {
             let result: Vec<Vec<f32>> = neighbours(&tr, &weights, &data, false);
             assert_eq!(result[24], vec![0f32]);
             assert_eq!(result[26], vec![12f32]);
+        }
+
+        {
+            let tr = Position{x:3, y:3, z:3};
+            let mut weights: Vec<Position> = Vec::new();
+            weights.push(Position { x: 2, y: 1, z: 1 });
+
+            let result: Vec<f32> = GeneratorIteratorAdapter(single_neighbours(&tr, &weights[0], &data, false)).collect();
+            assert_eq!(result[24], 0f32);
+            assert_eq!(result[26], 12f32);
         }
     }
 }
