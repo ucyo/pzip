ed16acedc3bc96ec604ff4475d6613decebaf221
ucyo <cayoglu@me.com>
2019-02-20 21:11:31 +0100

    Add full logs for delta correction

--- examples/correction.rs
+++ examples/correction.rs
@@ -5,7 +5,6 @@ pub trait CorrectionTrait {
     fn apply_correction(&mut self, pred: &u32) -> u32;
 }
 
-
 /// PreviousError correction
 ///
 /// ## tl;dr
@@ -23,8 +22,8 @@ pub trait CorrectionTrait {
 pub struct PreviousError {
     overshot: bool,
     offset: u32,
-    beta: u32,   // relative of parts
-    parts: u32,  // absolute parts [default: 100]
+    beta: u32,  // relative of parts
+    parts: u32, // absolute parts [default: 100]
 }
 
 impl PreviousError {
@@ -57,9 +56,9 @@ impl CorrectionTrait for PreviousError {
         if self.overshot {
             if correction > *pred {
                 self.offset = 0;
-                return 0
+                return 0;
             } else {
-                return pred - correction
+                return pred - correction;
             }
         } else {
             pred + correction
@@ -70,10 +69,13 @@ impl CorrectionTrait for PreviousError {
 use std::fmt;
 impl fmt::Display for PreviousError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "PreviousError {{ overshot: {}, offset: {:b} }}", self.overshot, self.offset)
+        write!(
+            f,
+            "PreviousError {{ overshot: {}, offset: {:b} }}",
+            self.overshot, self.offset
+        )
     }
 }
-
 
 /// DeltaToPowerOf2
 ///
@@ -99,7 +101,11 @@ pub struct DeltaToPowerOf2 {
 
 impl fmt::Display for DeltaToPowerOf2 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "DeltaToPowerOf2 {{ overshot: {}, restricted: {}, delta: {} }}", self.overshot, self.restricted, self.delta)
+        write!(
+            f,
+            "DeltaToPowerOf2 {{ overshot: {}, restricted: {}, delta: {} }}",
+            self.overshot, self.restricted, self.delta
+        )
     }
 }
 
@@ -145,7 +151,7 @@ impl CorrectionTrait for DeltaToPowerOf2 {
 }
 
 fn delta_to_next_power_of_two(val: u32, pos: u32) -> u32 {
-    let pos = if pos > 0 {pos - 1} else {0};
+    let pos = if pos > 0 { pos - 1 } else { 0 };
     let val = val << pos >> pos;
     val.next_power_of_two() - val
 }
@@ -156,55 +162,75 @@ fn delta_to_former_power_of_two(val: u32, pos: u32) -> u32 {
     val - (val.next_power_of_two() >> 1)
 }
 
+use log::{debug, error, info, trace, warn};
+use pzip::testing::{FileToBeCompressed, Source};
 #[allow(unused_variables)]
 use std::env::args;
-use pzip::testing::{Source, FileToBeCompressed};
-use log::{trace, debug, info, warn, error};
 fn main() {
     env_logger::init();
     let arguments: Vec<_> = args().collect();
     let filename = &arguments[1];
-    let mut source : Source<f32> = Source::new(filename);
+    let mut source: Source<f32> = Source::new(filename);
     source.load().unwrap();
     let data: Vec<u32> = source.data.iter().map(|x| x.to_bits()).collect();
-    let data = data[303568..303568+10].to_vec();
-    // let (start, size) = (90300, 9000);
-    // let data = data[start..start+size].to_vec();
+    let (start, size) = (303568, 100);
+    let data = data[start..start + size].to_vec();
     // let data: Vec<u32> = vec![4,5,6,8,10,9,0];
     let mut uncorrected_last_value_prediction = vec![0u32; data.len()];
     for v in 1..data.len() {
-        uncorrected_last_value_prediction[v] = data[v-1];
+        uncorrected_last_value_prediction[v] = data[v - 1];
     }
 
-    let mut corrected_last_value_prediction : Vec<u32> = Vec::new();
+    let mut corrected_last_value_prediction: Vec<u32> = Vec::new();
     let mut pred = 0u32;
     let mut method = DeltaToPowerOf2::new();
 
-    for (i,value) in data.iter().enumerate() {
+    for (i, value) in data.iter().enumerate() {
         let uncorrected = pred;
         pred = method.apply_correction(&pred);
         debug!("IX: {:08} Uncorrected: {:032b}", i, uncorrected);
         debug!("IX: {:08}   Corrected: {:032b} by {}", i, pred, method);
         debug!("IX: {:08}       Truth: {:032b}", i, value);
-        let (before, after) = ((uncorrected ^ value).leading_zeros(), (pred ^ value).leading_zeros());
+        let (before, after) = (
+            (uncorrected ^ value).leading_zeros(),
+            (pred ^ value).leading_zeros(),
+        );
         if before > after {
-         warn!("              Degradation: {:02}", before - after);
+            warn!("              Degradation: {:02}", before - after);
+        } else if before == after {
+            debug!("               No change: {:02}", after);
         } else {
-         debug!("             Improvement: {:02}", after - before);
+            debug!("             Improvement: {:02}", after - before);
         }
         corrected_last_value_prediction.push(pred);
         method.calculate_offset(value, &pred);
         pred = *value;
     }
 
-    let lzc_data : u32 = data.iter().map(|x| x.leading_zeros()).sum();
-    let lzc_uncorrected_pred: Vec<u32> = data.iter().zip(uncorrected_last_value_prediction.iter()).map(|(t,p)| (t^p).leading_zeros()).collect();
-    let lzc_uncorrected_pred_sum : u32 = lzc_uncorrected_pred.iter().sum();
-    let lzc_corrected_pred: Vec<u32> = data.iter().zip(corrected_last_value_prediction.iter()).map(|(t,p)| (t^p).leading_zeros()).collect();
-    let lzc_corrected_pred_sum : u32 = lzc_corrected_pred.iter().sum();
+    let lzc_data: u32 = data.iter().map(|x| x.leading_zeros()).sum();
+    let lzc_uncorrected_pred: Vec<u32> = data
+        .iter()
+        .zip(uncorrected_last_value_prediction.iter())
+        .map(|(t, p)| (t ^ p).leading_zeros())
+        .collect();
+    let lzc_uncorrected_pred_sum: u32 = lzc_uncorrected_pred.iter().sum();
+    let lzc_corrected_pred: Vec<u32> = data
+        .iter()
+        .zip(corrected_last_value_prediction.iter())
+        .map(|(t, p)| (t ^ p).leading_zeros())
+        .collect();
+    let lzc_corrected_pred_sum: u32 = lzc_corrected_pred.iter().sum();
 
-    let same : u32 = lzc_corrected_pred.iter().zip(lzc_uncorrected_pred.iter()).filter(|(c,u)| c == u).fold(0, |sum, _| sum + 1 );
-    let better : u32 = lzc_corrected_pred.iter().zip(lzc_uncorrected_pred.iter()).filter(|(c,u)| c > u).fold(0, |sum, _| sum + 1 );
+    let same: u32 = lzc_corrected_pred
+        .iter()
+        .zip(lzc_uncorrected_pred.iter())
+        .filter(|(c, u)| c == u)
+        .fold(0, |sum, _| sum + 1);
+    let better: u32 = lzc_corrected_pred
+        .iter()
+        .zip(lzc_uncorrected_pred.iter())
+        .filter(|(c, u)| c > u)
+        .fold(0, |sum, _| sum + 1);
     let worse = data.len() as u32 - same - better;
 
     let sum_bits = data.len() * 32;
@@ -212,15 +238,38 @@ fn main() {
     info!("FILE: {}", filename);
     info!("====");
     info!("TOTL: {}", sum_bits);
-    info!("ORIG: {} ({:.4}%)", lzc_data, lzc_data as f32/sum_bits as f32 * 100.0);
-    info!("UNCO: {} ({:.4}%)", lzc_uncorrected_pred_sum, lzc_uncorrected_pred_sum as f32/sum_bits as f32 * 100.0);
-    info!("CORR: {} ({:.4}%)", lzc_corrected_pred_sum, lzc_corrected_pred_sum as f32/sum_bits as f32 * 100.0);
+    info!(
+        "ORIG: {} ({:.4}%)",
+        lzc_data,
+        lzc_data as f32 / sum_bits as f32 * 100.0
+    );
+    info!(
+        "UNCO: {} ({:.4}%)",
+        lzc_uncorrected_pred_sum,
+        lzc_uncorrected_pred_sum as f32 / sum_bits as f32 * 100.0
+    );
+    info!(
+        "CORR: {} ({:.4}%)",
+        lzc_corrected_pred_sum,
+        lzc_corrected_pred_sum as f32 / sum_bits as f32 * 100.0
+    );
     info!("====");
-    info!("SAME: {} ({:.4}%)", same, same as f32 / data.len() as f32 * 100.0);
-    info!("BETT: {} ({:.4}%)", better, better as f32 / data.len() as f32 * 100.0);
-    info!("WORS: {} ({:.4}%)", worse, worse as f32 / data.len() as f32 * 100.0);
+    info!(
+        "SAME: {} ({:.4}%)",
+        same,
+        same as f32 / data.len() as f32 * 100.0
+    );
+    info!(
+        "BETT: {} ({:.4}%)",
+        better,
+        better as f32 / data.len() as f32 * 100.0
+    );
+    info!(
+        "WORS: {} ({:.4}%)",
+        worse,
+        worse as f32 / data.len() as f32 * 100.0
+    );
     // info!("{:?}\t data", data);
     // info!("{:?}\t pred", uncorrected_last_value_prediction);
     // info!("{:?}\t corr pred", corrected_last_value_prediction);
-
 }
