a6247b15812f44360c198d619cbd7c213d773411
ucyo <cayoglu@me.com>
2019-04-12 20:47:26 +0200

    Consider different shift value if gray codes are being used

--- src/residual.rs
+++ src/residual.rs
@@ -1,9 +1,25 @@
 const ONE_ZERO_U32: u32 = 2863311530;
 const ZERO_ONE_U32: u32 = 1431655765;
+const ZERO_ONES: (u32,u32) = (2863311530, 1431655765);
+// const GRAY_ONE_ZERO_U32: u32 = (4294967295 >> 11) + (3 << 30);
+// const GRAY_ZERO_ONE_U32: u32 = (2147483647 >> 11) + (3 << 30);
+// const GRAY_ONE_ZERO_U32: u32 = (3 << 30);
+// const GRAY_ZERO_ONE_U32: u32 = (3 << 30);
+const GRAY_ONE_ZERO_U32: u32 = 3435973836;
+const GRAY_ZERO_ONE_U32: u32 = 858993459;
 // TODO: Current implmentation only supports u32 values. Add to this u64.
 // const ONE_ZERO_U64: u64 = 12297829382473034410;
 // const ZERO_ONE_U64: u64 = 6148914691236517205;
 use log::{debug, warn};
+
+const BINARY_GOAL: GoalBase = GoalBase { one_zero: 2863311530, zero_one: 1431655765 };
+const GRAY_GOAL: GoalBase = GoalBase { one_zero: 3 << 30, zero_one: 3 << 30 };
+
+#[derive(Debug)]
+struct GoalBase {
+    one_zero: u32,
+    zero_one: u32
+}
 
 #[derive(Debug)]
 pub struct RContext {
@@ -29,8 +45,8 @@ pub enum ResidualCalculation {
     ExclusiveOR,
     Shifted,
     ShiftedLZC,
+    ShiftedGray,
     // TODO: Choose a goal for shifted value solely based on the prediction
-    // TODO: Choose a goal for the shifted value with the knowledge that a grey code will be used
     // TODO: Choose residual based on experience (past values) instead of LZC or given cut
     // TODO: Choose cut based on length of ones/zeros and(!) decimal point of these positions in floating point representation
     // TODO: Choose shift in such a way that the former LZC position will be a 1
@@ -47,6 +63,18 @@ impl ResidualTrait for ResidualCalculation {
             },
             ResidualCalculation::Shifted => {
                 let (add, shift) = shift_calculation(*prediction, rctx);
+                let shifted_prediction = apply_shift(*prediction, &add, &shift);
+                let shifted_truth = apply_shift(*truth, &add, &shift);
+                let result = shifted_prediction ^ shifted_truth;
+                debug!("Panic?\n T{:032b}\n P{:032b}\nST{:032b}\nSP{:032b}\n X{:032b}\nSX{:032b}\n", *truth, *prediction, shifted_truth, shifted_prediction, (truth ^ prediction), result);
+                debug!("New: {} Old: {}", result.leading_zeros(), (truth ^ prediction).leading_zeros());
+                if result.leading_zeros() < (truth ^ prediction).leading_zeros() - 1 {
+                    warn!("LZC worse using shift by {}", (truth ^ prediction).leading_zeros() - result.leading_zeros());
+                }
+                result
+            },
+            ResidualCalculation::ShiftedGray => {
+                let (add, shift) = shift_calculation_gray(*prediction, rctx);
                 let shifted_prediction = apply_shift(*prediction, &add, &shift);
                 let shifted_truth = apply_shift(*truth, &add, &shift);
                 let result = shifted_prediction ^ shifted_truth;
@@ -81,7 +109,14 @@ impl ResidualTrait for ResidualCalculation {
                 let shifted_truth = *residual ^ shifted_prediction;
                 let truth = apply_shift(shifted_truth, &!add, &shift);
                 truth
-            }
+            },
+            ResidualCalculation::ShiftedGray => {
+                let (add, shift) = shift_calculation_gray(*prediction, rctx);
+                let shifted_prediction = apply_shift(*prediction, &add, &shift);
+                let shifted_truth = *residual ^ shifted_prediction;
+                let truth = apply_shift(shifted_truth, &!add, &shift);
+                truth
+            },
             ResidualCalculation::ShiftedLZC => {
                 let (add, shift) = shift_calculation(*prediction, rctx);
                 let shifted_prediction = apply_shift(*prediction, &add, &shift);
@@ -95,10 +130,13 @@ impl ResidualTrait for ResidualCalculation {
         match self {
             ResidualCalculation::ExclusiveOR => {
                 rctx.prediction = *prediction;
-                rctx.truth = *truth;}
+                rctx.truth = *truth;},
             ResidualCalculation::Shifted => {
                 rctx.prediction = *prediction;
-                rctx.truth = *truth;}
+                rctx.truth = *truth;},
+            ResidualCalculation::ShiftedGray => {
+                rctx.prediction = *prediction;
+                rctx.truth = *truth;},
             ResidualCalculation::ShiftedLZC => {
                 rctx.prediction = *prediction;
                 rctx.truth = *truth;
@@ -121,6 +159,25 @@ fn shift_calculation(num: u32, rctx: &mut RContext) -> (bool, u32) {
         return (num <= goal, shift);
     } else {
         let delta = ONE_ZERO_U32 >> (bits - rctx.cut);
+        let goal = base + delta;
+        let shift = num.max(goal) - num.min(goal);
+        debug!("Shift {0:032b} @ {1} by {2:032b} to {3:032b} t", num, rctx.cut, shift, goal);
+        return (num < goal, shift);
+    }
+}
+
+fn shift_calculation_gray(num: u32, rctx: &mut RContext) -> (bool, u32) {
+    let bits = 32;
+    let base = (num >> rctx.cut) << rctx.cut;
+    let last_value = (num >> rctx.cut) & 1;
+    if last_value == 1 {
+        let delta = GRAY_ZERO_ONE_U32 ;//>> (bits - rctx.cut);
+        let goal = base + delta;
+        let shift = num.max(goal) - num.min(goal);
+        debug!("Shift {0:032b} @ {1} by {2:032b} to {3:032b} f", num, rctx.cut, shift, goal);
+        return (num <= goal, shift);
+    } else {
+        let delta = GRAY_ONE_ZERO_U32;// >> (bits - rctx.cut);
         let goal = base + delta;
         let shift = num.max(goal) - num.min(goal);
         debug!("Shift {0:032b} @ {1} by {2:032b} to {3:032b} t", num, rctx.cut, shift, goal);
